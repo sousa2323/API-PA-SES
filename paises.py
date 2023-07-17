c70bd60b0760b772666c20181633a0be15896df4
sousa2323 <sousanot.23@gmail.com>
2023-07-16 22:47:08 -0300

    refatorando codigo

--- paises.py
+++ paises.py
@@ -3,14 +3,52 @@ import json
 import requests
 
 URL_ALL = "https://restcountries.com/v2/all"
-URL_NAME = "https://restcountries.com/v2/name/brazil"
+URL_NAME = "https://restcountries.com/v2/name"
 
-resposta = requests.get(URL_ALL)
+def requisicao(url):
+    try:
+        resposta = requests.get(url)
+        if resposta.status_code == 200:
+            return resposta.text
+    except:
+        print("Erro ao fazer requisição em:", url)
 
+def parsing(texto_da_resposta):
+    try:
+        return json.loads(texto_da_resposta)
+    except:
+        print("Erro ao fazer parsing")        
 
-paises = json.loads(resposta.text) # PARSING de JSON para PYTHON
+def contagem_de_paises(lista_de_paises):
+    return len(lista_de_paises)
 
-print(len(paises))
+def listar_paises(lista_de_paises):
+    for pais in lista_de_paises:
+        print(pais['name'])
 
-for pais in paises:
-    print(pais['currencies'])
+def mostrar_populacao(nome_do_pais):
+    resposta = lista_de_paises = requisicao("{}/{}".format(URL_NAME, nome_do_pais))
+    if resposta:
+        lista_de_paises = parsing(resposta)
+        if lista_de_paises:
+            for pais in lista_de_paises:
+                print("{}: {}".format(pais['name'], pais['population']))
+        else:
+            print("Pais não encontrado")
+
+def mostrar_moedas(nome_do_pais):
+    resposta = lista_de_paises = requisicao("{}/{}".format(URL_NAME, nome_do_pais))
+    if resposta:
+        lista_de_paises = parsing(resposta)
+        if lista_de_paises:
+            for pais in lista_de_paises:
+                print("Moedas do", pais['name'])
+                moedas = pais['currencies']
+                for moeda in moedas:
+                    print("{} - {}".format(moeda['name'], moeda['code']))
+        else:
+            print("Pais não encontrado")
+
+if __name__ == "__main__":
+    # mostrar_populacao('brazil')
+    mostrar_moedas('bra')
