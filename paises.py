7631da29e979cfdb8d685e334e6a534ba681e2b0
sousa2323 <sousanot.23@gmail.com>
2023-07-16 23:52:57 -0300

    Passagem de argumentos por linha de comando

--- paises.py
+++ paises.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 import requests
 
@@ -20,21 +21,25 @@ def parsing(texto_da_resposta):
         print("Erro ao fazer parsing")        
 
 def contagem_de_paises(lista_de_paises):
-    return len(lista_de_paises)
+    resposta = requisicao(URL_ALL)
+    if resposta:
+        lista_de_paises = parsing(resposta)
+        if lista_de_paises:
+            return len(lista_de_paises)
 
 def listar_paises(lista_de_paises):
     for pais in lista_de_paises:
         print(pais['name'])
 
 def mostrar_populacao(nome_do_pais):
-    resposta = lista_de_paises = requisicao("{}/{}".format(URL_NAME, nome_do_pais))
+    resposta = requisicao("{}/{}".format(URL_NAME, nome_do_pais))
     if resposta:
         lista_de_paises = parsing(resposta)
         if lista_de_paises:
             for pais in lista_de_paises:
-                print("{}: {}".format(pais['name'], pais['population']))
-        else:
-            print("Pais não encontrado")
+                print("{}: {} habitantes".format(pais['name'], pais['population']))
+    else:
+        print("País não encontrado")
 
 def mostrar_moedas(nome_do_pais):
     resposta = lista_de_paises = requisicao("{}/{}".format(URL_NAME, nome_do_pais))
@@ -46,9 +51,34 @@ def mostrar_moedas(nome_do_pais):
                 moedas = pais['currencies']
                 for moeda in moedas:
                     print("{} - {}".format(moeda['name'], moeda['code']))
-        else:
-            print("Pais não encontrado")
+    else:
+        print("País não encontrado")
+
+def ler_nome_do_pais():
+    try:
+        nome_do_pais = sys.argv[2]
+        return nome_do_pais
+    except:
+        print("É preciso passar o nome do país")
 
 if __name__ == "__main__":
-    # mostrar_populacao('brazil')
-    mostrar_moedas('bra')
+    if len(sys.argv) == 1:
+        print("## Bem vindo ao sistema de países ##")
+        print("Uso: python paises.py <ação> <nome do país>")
+        print("Ações disponíveis: contagem, moeda, população")
+    else:
+        argumento1 = sys.argv[1]
+        
+        if argumento1 == "contagem":
+            numero_de_paises = contagem_de_paises()
+            print("Existem {} países no mundo todo".format(numero_de_paises))
+        elif argumento1 == "moeda":
+            pais = ler_nome_do_pais()
+            if pais:
+                mostrar_moedas(pais)
+        elif argumento1 == "populacao":
+            pais = ler_nome_do_pais()
+            if pais:
+                mostrar_populacao(pais)
+        else:
+            print("Argumento inválido")
